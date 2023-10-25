00b3f2af4a550a7a7f2bd87afb3c55a47c1c7118
joshirishi <joshi.rishikesh@gmail.com>
2023-10-25 11:51:39 +0530

    Tried few approaches
    going into mtbe for server interaction in real time. Will save it as an mtbe branch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,131 @@
 const puppeteer = require('puppeteer');
 const axios = require('axios');
 
+const targetUrl = process.argv[2] || 'https://maitridesigns.com'; // Use passed URL or default to maitridesigns.com
+const maxDepth = 2; // Limit the depth of scraping to avoid too many requests
+
+async function scrapeWebMap(url, depth = 0) {
+    if (depth > maxDepth) {
+        return { id: url };
+    }
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto(url, { waitUntil: 'networkidle2' });
+
+    const links = await page.$$eval('a', anchors => {
+        return anchors.map(anchor => anchor.href);
+    });
+
+    await browser.close();
+
+    const uniqueLinks = [...new Set(links)]; // Remove duplicates
+    const children = [];
+
+    for (let link of uniqueLinks) {
+        if (link.startsWith(targetUrl)) { // Only follow internal links
+            const childData = await scrapeWebMap(link, depth + 1);
+            children.push(childData);
+        }
+    }
+
+    return {
+        id: url,
+        children: children.length > 0 ? children : undefined
+    };
+}
+
+async function storeWebMapData(data) {
+    try {
+        const response = await axios.post('http://localhost:8000/api/store-webmap', data);
+        console.log('Data stored:', response.data);
+    } catch (error) {
+        console.error('Error storing web map data:', error);
+    }
+}
+
+async function main() {
+    const webMapData = await scrapeWebMap(targetUrl);
+    await storeWebMapData(webMapData);
+}
+
+main();
+
+
+
+/*
+const puppeteer = require('puppeteer');
+const axios = require('axios');
+
+// Function to introduce a delay (for rate limiting)
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Function to scrape links and titles from a given webpage
+async function scrapeLinks(url) {
+    const browser = await puppeteer.launch({ headless: "new" });
+    const page = await browser.newPage();
+
+    try {
+        await page.goto(url, { waitUntil: 'networkidle2' });
+        const links = await page.$$eval('a', anchors => {
+            return anchors.map(anchor => ({ href: anchor.href, title: anchor.textContent.trim() }))
+                          .filter(link => link.href.startsWith('https://maitridesigns.com/'));  // Filter links to only include those within the domain
+        });
+
+        await browser.close();
+        return links;
+    } catch (error) {
+        console.error(`Failed to scrape ${url}: ${error.message}`);
+        await browser.close();
+        return [];
+    }
+}
+
+// Recursive function to build the web map in a hierarchical manner
+async function buildWebMap(url, visited = new Set(), depth = 0, maxDepth = 2) {
+    if (depth > maxDepth || visited.has(url)) return null;
+
+    visited.add(url);
+
+    const links = await scrapeLinks(url);
+    let children = [];
+
+    for (let linkObj of links) {
+        const childData = await buildWebMap(linkObj.href, visited, depth + 1, maxDepth);
+        if (childData) {
+            children.push(childData);
+        }
+    }
+
+    return {
+        url: url,
+        title: links.find(link => link.href === url)?.title || '',
+        links: children
+    };
+}
+
+buildWebMap('https://maitridesigns.com', new Set(), 0, 2).then(webMap => {
+    // Send the hierarchical webMap to the backend
+    axios.post('http://localhost:8000/api/store-webmap', webMap)
+         .then(response => {
+             console.log(`Data sent to backend:`, response.data);
+         })
+         .catch(error => {
+             console.error(`Error sending data to backend:`, error);
+         });
+});
+
+module.exports = buildWebMap;
+
+
+*/
+/* 
+const puppeteer = require('puppeteer');
+const axios = require('axios');
+
 // Function to introduce a delay (for rate limiting)
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -68,3 +193,5 @@ buildWebMap('https://maitridesigns.com', 2).then(webMap => {
 
 
 module.exports = buildWebMap;
+
+*/
--- src/network_chart.js
+++ src/network_chart.js
@@ -1,3 +1,289 @@
+const DEFAULT_URL = 'https://maitridesigns.com';  // Define a default URL at the top of your script
+async function fetchData() {
+    try {
+        const response = await fetch('http://localhost:8000/api/get-webmap');
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching web map data:", error);
+    }
+}
+
+function renderNetworkChart(data) {
+    const width = 800;
+    const height = 600;
+
+    const svg = d3.select("#network-chart")
+                  .append("svg")
+                  .attr("width", width)
+                  .attr("height", height);
+
+    const root = d3.hierarchy(data);
+    const links = root.links();
+    const nodes = root.descendants();
+
+    const simulation = d3.forceSimulation(nodes)
+                         .force("link", d3.forceLink(links).id(d => d.id).distance(50))
+                         .force("charge", d3.forceManyBody().strength(-50))
+                         .force("center", d3.forceCenter(width / 2, height / 2));
+
+    const link = svg.append("g")
+                    .attr("stroke", "#999")
+                    .attr("stroke-opacity", 0.6)
+                    .selectAll("line")
+                    .data(links)
+                    .join("line");
+
+    const node = svg.append("g")
+                    .attr("stroke", "#fff")
+                    .attr("stroke-width", 1.5)
+                    .selectAll("circle")
+                    .data(nodes)
+                    .join("circle")
+                    .attr("r", 5)
+                    .attr("fill", "blue")
+                    .call(d3.drag()
+                          .on("start", dragstarted)
+                          .on("drag", dragged)
+                          .on("end", dragended))
+                    .on("click", nodeClicked);
+
+    node.append("title")
+        .text(d => d.data.id);
+
+    simulation.on("tick", () => {
+        link.attr("x1", d => d.source.x)
+            .attr("y1", d => d.source.y)
+            .attr("x2", d => d.target.x)
+            .attr("y2", d => d.target.y);
+
+        node.attr("cx", d => d.x)
+            .attr("cy", d => d.y);
+    });
+
+    function dragstarted(event) {
+        if (!event.active) simulation.alphaTarget(0.3).restart();
+        event.subject.fx = event.subject.x;
+        event.subject.fy = event.subject.y;
+    }
+
+    function dragged(event) {
+        event.subject.fx = event.x;
+        event.subject.fy = event.y;
+    }
+
+    function dragended(event) {
+        if (!event.active) simulation.alphaTarget(0);
+        event.subject.fx = null;
+        event.subject.fy = null;
+    }
+
+    function nodeClicked(event, d) {
+        // Use the node's URL if it exists, otherwise use the default URL
+        const urlToScrape = d.data.id || DEFAULT_URL;
+        triggerScraper(urlToScrape);
+    }
+}
+
+async function triggerScraper(url) {
+    try {
+        const response = await fetch(`http://localhost:8000/api/trigger-scraper?url=${url}`);
+        const data = await response.json();
+        console.log("Scraper response:", data);
+    } catch (error) {
+        console.error("Error triggering the scraper:", error);
+    }
+}
+
+async function init() {
+    const webMapData = await fetchData();
+    if (webMapData && webMapData.length > 0) {
+        renderNetworkChart(webMapData[0]); // Assuming the first item is the root node
+    } else {
+        console.error("Failed to fetch web map data or data structure is incorrect.");
+    }
+}
+
+init();
+
+
+/*
+((async function() {
+    const targetUrl = 'https://maitridesigns.com';
+
+    async function fetchData() {
+        const response = await fetch("http://localhost:8000/api/get-webmap");
+
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+
+        const webMapData = await response.json();
+
+        console.log("Server Response:", webMapData);
+
+        if (!Array.isArray(webMapData)) {
+            throw new Error("Data is not in the expected format");
+        }
+
+        return webMapData;
+    }
+
+    const rawData = await fetchData();
+
+    if (!rawData || rawData.length === 0) {
+        console.error('Failed to fetch data.');
+        return;
+    }
+    console.log("Raw Data:", rawData);
+
+    const data = transformData(rawData[0]); // Assuming you want the first item from the array
+    console.log("Transformed Data:", data);
+
+    const width = 928;
+    const height = 600;
+    const color = d3.scaleOrdinal(d3.schemeCategory10);
+
+    const links = data.links.map(d => ({...d}));
+    const nodes = data.nodes.map(d => ({...d}));
+
+    const simulation = d3.forceSimulation(nodes)
+        .force("link", d3.forceLink(links).id(d => d.id).distance(100))
+        .force("charge", d3.forceManyBody().strength(-1000))
+        .force("center", d3.forceCenter(width / 2, height / 2))
+        .on("tick", draw);
+
+    const canvas = d3.select("body").append("canvas")
+        .attr("width", width)
+        .attr("height", height)
+        .node();
+
+    const context = canvas.getContext("2d");
+
+    function draw() {
+        context.clearRect(0, 0, width, height);
+        context.save();
+        context.globalAlpha = 0.6;
+        context.strokeStyle = "#999";
+        context.beginPath();        
+        links.forEach(drawLink);
+        context.stroke();
+        context.restore();
+
+        context.save();
+        nodes.forEach(node => {
+            context.beginPath();
+            drawNode(node);
+            context.fillStyle = color(node.group);
+            context.strokeStyle = "#fff";
+            context.fill();
+            context.stroke();
+        });
+        context.restore();
+    }
+
+    function drawLink(d) {
+        context.moveTo(d.source.x, d.source.y);
+        context.lineTo(d.target.x, d.target.y);
+    }
+
+    function drawNode(d) {
+        context.moveTo(d.x + 5, d.y);
+        context.arc(d.x, d.y, 5, 0, 2 * Math.PI);
+    }
+
+    d3.select(canvas)
+        .call(d3.drag()
+            .subject(event => {
+                const [px, py] = d3.pointer(event, canvas);
+                return d3.least(nodes, ({x, y}) => {
+                    const dist2 = (x - px) ** 2 + (y - py) ** 2;
+                    if (dist2 < 400) return dist2;
+                });
+            })
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended));
+
+    function dragstarted(event) {
+        if (!event.active) simulation.alphaTarget(0.3).restart();
+        event.subject.fx = event.subject.x;
+        event.subject.fy = event.subject.y;
+    }
+
+    function dragged(event) {
+        event.subject.fx = event.x;
+        event.subject.fy = event.y;
+    }
+
+    function dragended(event) {
+        if (!event.active) simulation.alphaTarget(0);
+        event.subject.fx = null;
+        event.subject.fy = null;
+    }
+
+    function transformData(data, parentId = null) {
+        let nodes = [];
+        let links = [];
+    
+        const nodeId = data.url;
+        nodes.push({ id: nodeId, title: data.title });
+    
+        if (parentId) {
+            links.push({ source: parentId, target: nodeId });
+        }
+    
+        // Check if data.links is defined and is an array
+        if (data.links && Array.isArray(data.links)) {
+            data.links.forEach(child => {
+                const childData = transformData(child, nodeId);
+                nodes = nodes.concat(childData.nodes);
+                links = links.concat(childData.links);
+            });
+        }
+    
+        return { nodes, links };
+    }
+})());
+    
+
+
+*/
+
+        /*function transformDataToHierarchy(data) {
+        let hierarchicalData = [];
+
+        data.forEach(item => {
+            hierarchicalData.push({
+                id: item.url,
+                parentId: getParentId(item.url),
+                name: extractName(item.url)
+            });
+            item.links.forEach(link => {
+                hierarchicalData.push({
+                    id: link,
+                    parentId: item.url,
+                    name: extractName(link)
+                });
+            });
+        });
+
+        return hierarchicalData;
+    }
+
+    function extractName(url) {
+        const parts = url.replace('https://maitridesigns.com/', '').split('/');
+        return parts[parts.length - 1] || 'maitridesigns.com';
+    }
+
+    function getParentId(url) {
+        const parts = url.replace('https://maitridesigns.com/', '').split('/');
+        parts.pop();
+        return parts.length ? `https://maitridesigns.com/${parts.join('/')}` : null;
+    }
+})();*/
+
+/*
 (async function() {
     const targetUrl = 'https://maitridesigns.com';
 
@@ -114,7 +400,7 @@
     }
 })();
 
-
+*/
 /*
 const targetUrl = 'https://maitridesigns.com';
 
@@ -242,8 +528,5 @@ function transformData(data) {
     return { nodes, links };
 <<<<<<< Updated upstream
 }*/
-=======
-}
 
-//neet to get the representation right
->>>>>>> Stashed changes
+
