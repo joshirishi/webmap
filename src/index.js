4eefd360a8502217e9a8aa3e32251ccf603fffb1
joshirishi <joshi.rishikesh@gmail.com>
2023-11-17 23:28:43 +0530

    new logic
    new logic with code helper, this one will scrape links from top part 200 px first and then go down. I thnk next iteration will be 200px consequtively downward scrape all the links. So each row is 200 px worth.  I am aiming to make the structure look as complete as possible.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,123 @@
 const puppeteer = require('puppeteer');
 const axios = require('axios');
+
+// Configuration
+const maxDepth = 30;
+const targetUrl = 'https://journeys-unlimited.com'; // Replace with your target URL
+const visitedUrls = new Set();
+const concurrentLimit = 5;
+let currentDepth = 0;
+
+// Initialize Puppeteer
+async function initBrowser() {
+    return puppeteer.launch({ headless: "new", args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+}
+
+// URL Normalization function
+function normalizeUrl(link) {
+    const urlObj = new URL(link);
+    // Remove query parameters and fragment identifiers
+    return urlObj.origin + urlObj.pathname;
+}
+
+// Function to check if URL is an HTML page (not PDF, image, JS, etc.)
+function isHtmlPage(url) {
+    return !/\.(pdf|jpg|jpeg|png|gif|svg|js|css)$/i.test(url);
+}
+
+// Extract and filter links based on their position on the page
+async function extractTopLinks(page, threshold = 200) {
+    return page.evaluate((threshold, baseDomain) => {
+        const links = Array.from(document.querySelectorAll('a[href]'))
+                        .map(a => {
+                            const rect = a.getBoundingClientRect();
+                            const href = new URL(a.href, document.baseURI).href;
+                            return { href, top: rect.top };
+                        })
+                        .filter(item => item.href.startsWith(baseDomain) && item.top <= threshold && !/\.(pdf|jpg|jpeg|png|gif|svg|js|css)$/i.test(item.href));
+        return links.map(item => item.href);
+    }, threshold, targetUrl);
+}
+
+// Main scraping function
+async function scrapeWebMap(currentUrl, depth) {
+    console.log(`Starting scrape of: ${currentUrl} at depth: ${depth}`);
+    if (depth > maxDepth || visitedUrls.has(currentUrl)) {
+        console.log(`Skipping ${currentUrl} (already visited or max depth reached)`);
+        return;
+    }
+
+    visitedUrls.add(currentUrl);
+
+    try {
+        const browser = await initBrowser();
+        const page = await browser.newPage();
+        await page.goto(currentUrl, { waitUntil: 'networkidle2' });
+
+        const pageData = await page.evaluate(() => {
+            const title = document.title;
+            const links = Array.from(document.querySelectorAll('a[href]'))
+                            .map(a => new URL(a.href, document.baseURI).href)
+                            .filter(href => href.startsWith('http') && !href.includes('#') && !/\.(pdf|jpg|jpeg|png|gif|svg|js|css)$/i.test(href));
+            return { title, links };
+        });
+
+        const children = [];
+        for (const link of pageData.links.filter(link => link.startsWith(targetUrl) && isHtmlPage(link))) {
+            const normalizedLink = normalizeUrl(link);
+            if (!visitedUrls.has(normalizedLink) && normalizedLink !== currentUrl) {
+                const childData = await scrapeWebMap(normalizedLink, depth + 1);
+                if (childData) {
+                    children.push(childData);
+                }
+            }
+        }
+
+        await browser.close();
+        console.log(`Completed scrape of: ${currentUrl}`);
+        return { name: pageData.title || currentUrl, url: currentUrl, children };
+    } catch (error) {
+        console.error(`Error scraping ${currentUrl}:`, error);
+        return null;
+    }
+}
+
+// Store data
+async function storeWebMapData(data) {
+    console.log(`Sending scraped data to database for URL: ${data.url}`);
+    try {
+        await axios.post('http://localhost:8000/api/store-webmap', data);
+        console.log(`Data successfully stored for URL: ${data.url}`);
+    } catch (error) {
+        console.error(`Error storing data for URL: ${data.url}:`, error);
+    }
+}
+
+// Main execution
+(async function main() {
+    console.log('Starting web scraping process...');
+    const browser = await initBrowser();
+    const page = await browser.newPage();
+    await page.goto(targetUrl, { waitUntil: 'networkidle2' });
+
+    // Extract and scrape top links
+    const topLinks = await extractTopLinks(page);
+    for (const link of topLinks) {
+        const scrapedData = await scrapeWebMap(link, currentDepth);
+        if (scrapedData) {
+            await storeWebMapData(scrapedData);
+        }
+    }
+
+    // Additional scraping logic for the rest of the website can be added here
+
+    await browser.close();
+    console.log('Web scraping process completed successfully.');
+})();
+
+/*
+const puppeteer = require('puppeteer');
+const axios = require('axios');
 const maxDepth = 3;
 const visitedUrls = new Set();
 const concurrentLimit = 5; // Limit the number of concurrent pages
@@ -105,148 +223,5 @@ async function main() {
 }
 
 main();
-
-
-/*
-const puppeteer = require('puppeteer');
-const axios = require('axios');
-
-// Function to introduce a delay (for rate limiting)
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-// Function to scrape links and titles from a given webpage
-async function scrapeLinks(url) {
-    const browser = await puppeteer.launch({ headless: "new" });
-    const page = await browser.newPage();
-
-    try {
-        await page.goto(url, { waitUntil: 'networkidle2' });
-        const links = await page.$$eval('a', anchors => {
-            return anchors.map(anchor => ({ href: anchor.href, title: anchor.textContent.trim() }))
-                          .filter(link => link.href.startsWith('https://maitridesigns.com/'));  // Filter links to only include those within the domain
-        });
-
-        await browser.close();
-        return links;
-    } catch (error) {
-        console.error(`Failed to scrape ${url}: ${error.message}`);
-        await browser.close();
-        return [];
-    }
-}
-
-// Recursive function to build the web map in a hierarchical manner
-async function buildWebMap(url, visited = new Set(), depth = 0, maxDepth = 2) {
-    if (depth > maxDepth || visited.has(url)) return null;
-
-    visited.add(url);
-
-    const links = await scrapeLinks(url);
-    let children = [];
-
-    for (let linkObj of links) {
-        const childData = await buildWebMap(linkObj.href, visited, depth + 1, maxDepth);
-        if (childData) {
-            children.push(childData);
-        }
-    }
-
-    return {
-        url: url,
-        title: links.find(link => link.href === url)?.title || '',
-        links: children
-    };
-}
-
-buildWebMap('https://maitridesigns.com', new Set(), 0, 2).then(webMap => {
-    // Send the hierarchical webMap to the backend
-    axios.post('http://localhost:8000/api/store-webmap', webMap)
-         .then(response => {
-             console.log(`Data sent to backend:`, response.data);
-         })
-         .catch(error => {
-             console.error(`Error sending data to backend:`, error);
-         });
-});
-
-module.exports = buildWebMap;
-
-
-*/
-/* 
-const puppeteer = require('puppeteer');
-const axios = require('axios');
-
-// Function to introduce a delay (for rate limiting)
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-// Function to scrape links and their titles from a given webpage
-async function scrapeLinks(url) {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    try {
-        await page.goto(url, { waitUntil: 'networkidle2' });
-        const data = await page.$$eval('a', anchors => {
-            return anchors.map(anchor => ({
-                href: anchor.href,
-                title: anchor.innerText.trim()
-            }));
-        });
-
-        await browser.close();
-        return data;
-    } catch (error) {
-        console.error(`Failed to scrape ${url}: ${error.message}`);
-        await browser.close();
-        return [];
-    }
-}
-
-// Function to build the web map
-async function buildWebMap(startUrl, depth = 2) {
-    let webMap = {};
-
-    async function crawl(url, level) {
-        if (level > depth) return;
-
-        const data = await scrapeLinks(url);
-        webMap[url] = data.map(linkObj => linkObj.href);  // Store only hrefs for compatibility with your backend
-
-        process.stdout.write('#');
-
-        // Use Promise.all() to handle multiple pages concurrently
-        const promises = [];
-        for (let linkObj of data) {
-            if (!webMap[linkObj.href]) {
-                promises.push(crawl(linkObj.href, level + 1));
-            }
-        }
-        await Promise.all(promises);
-    }
-
-    await crawl(startUrl, 1);
-    console.log(`\nCrawling completed.`);
-    return webMap;
-}
-
-buildWebMap('https://maitridesigns.com', 2).then(webMap => {
-    // Iterate over the webMap object and send each URL and its links to the backend
-    for (let [url, links] of Object.entries(webMap)) {
-        axios.post('http://localhost:8000/api/store-webmap', { url: url, links: links })
-             .then(response => {
-                 console.log(`Data for ${url} sent to backend:`, response.data);
-             })
-             .catch(error => {
-                 console.error(`Error sending data for ${url} to backend:`, error);
-             });
-    }
-});
-
-module.exports = buildWebMap;
 
 */
