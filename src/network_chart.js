614c7cb6c011527d8666e905bb2eb57c2b83e8d0
joshirishi <joshi.rishikesh@gmail.com>
2023-11-28 16:41:27 +0530

    added node color
    node color was in a branch put that only file back in main. Altho its not showing as it is supposed to ... nevertheless

--- src/network_chart.js
+++ src/network_chart.js
@@ -1,7 +1,23 @@
-
-
 (async function() {
-    const websiteId = 'journeys-unlimited.com-username'; // Replace with your website identifier
+    const websiteId = 'example.com-username'; // Replace with your website identifier
+    // Base color in HSL format
+    const baseHue = 135; // Example: blue hue
+    const baseSaturation = 63; // Percentage
+
+    function shadeColorByDepth(depth) {
+        // Adjust lightness based on depth (deeper nodes are darker)
+        const lightness = 43 - depth ;
+        return `hsl(${baseHue}, ${baseSaturation}%, ${lightness}%)`;
+    }
+
+    // Variables for customization
+    let a = "#96A621"; // Normal node color
+    let b = "#FF5733"; // Root node color
+    let c = 20;        // Node size
+    let d = "#000000";    // Link color
+    let e = 0.5;         // Link width
+    let f = 5.2;       // Rectangle width multiplier
+    let g = 20;        // Maximum text length
 
     async function fetchData() {
         try {
@@ -21,26 +37,24 @@
     }
 
     const data = rawData[0];
-    
-    // Specify the chart’s dimensions.
-    const width = 1920;
-    const height = 1080;
+    const width = 1160;
+    const height = 700;
 
-    // Compute the graph and start the force simulation.
     const root = d3.hierarchy(data); 
     const links = root.links();
     const nodes = root.descendants();
+     // Modify the color function to use the shadeColorByDepth function
+     const color = (d) => {
+        return shadeColorByDepth(d.depth);
+    };
 
-   
     const simulation = d3.forceSimulation(nodes)
-    .force("link", d3.forceLink(links).id(d => d.id).distance(100))
-    .force("charge", d3.forceManyBody().strength(-50)) // Reduced strength for less repulsion
-    .force("center", d3.forceCenter(width / 2, height / 2)) // Centering force
+        .force("link", d3.forceLink(links).id(d => d.id).distance(100))
+        .force("charge", d3.forceManyBody().strength(-50))
+        .force("center", d3.forceCenter(width / 2, height / 2))
+        .force("collide", d3.forceCollide(d => Math.max(d.data.name.length * 1, c)));
 
-        .force("collide", d3.forceCollide(d => Math.max(d.data.name.length * 1, 20))); // Collision based on rectangle size
-
-    // Create the container SVG and group for holding the graph
-        const svg = d3.select("body").append("svg")
+    const svg = d3.select("body").append("svg")
         .attr("width", width)
         .attr("height", height)
         .call(d3.zoom().on("zoom", (event) => {
@@ -48,19 +62,17 @@
         }))
         .append("g");
 
-        const graphGroup = svg.append("g");
+    const graphGroup = svg.append("g");
 
-    // Append links to the graph group
-        const link = graphGroup.append("g")
-        .attr("stroke", "#999")
-        .attr("stroke-width", 2)
-        .attr("stroke-opacity", 0.6)
+    const link = graphGroup.append("g")
+        .attr("stroke", d)
+        .attr("stroke-width", e)
+        .attr("stroke-opacity", 1)
         .selectAll("line")
         .data(links)
         .join("line");
-           
-    // Append node groups (for both rectangle and text)
-        const node = graphGroup.append("g")
+
+    const node = graphGroup.append("g")
         .attr("stroke", "#fff")
         .attr("stroke-width", 0)
         .selectAll("g")
@@ -68,51 +80,41 @@
         .join("g")
         .call(drag(simulation))
         .on('dblclick', (event, d) => {
-            window.open('about:blank', '_blank'); // Opens a new blank page
-            // If you want to open a specific URL, replace 'about:blank' with the URL
+            window.open('about:blank', '_blank');
         })
-    
         .on('mouseover', (event, d) => {
             addTooltip(nodeHoverTooltip, d, event.pageX, event.pageY);
         });
 
+ 
+    function drag(simulation) {
+        function dragstarted(event, d) {
+            if (!event.active) simulation.alphaTarget(0.3).restart();
+            d.fx = d.x;
+            d.fy = d.y;
+        }
 
-    // Define the color function
-    const color = (d) => {
-        return "#96A621";
-    };
+        function dragged(event, d) {
+            d.fx = event.x;
+            d.fy = event.y;
+        }
 
-    // Define the drag behavior
-function drag(simulation) {
-    function dragstarted(event, d) {
-        if (!event.active) simulation.alphaTarget(0.3).restart(); // Reduced alpha target for less bounciness
-        d.fx = d.x; // Fixing node position on drag start
-        d.fy = d.y;
+        function dragended(event, d) {
+            if (!event.active) simulation.alphaTarget(0);
+            d.fx = d.x;
+            d.fy = d.y;
+        }
+
+        return d3.drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended);
     }
 
-    function dragged(event, d) {
-        d.fx = event.x; // Fixing node position while dragging
-        d.fy = event.y;
-    }
-
-    function dragended(event, d) {
-        if (!event.active) simulation.alphaTarget(0); // Setting alpha target back to zero
-        d.fx = d.x; // Node remains at the position where it was dragged
-        d.fy = d.y;
-    }
-
-    return d3.drag()
-        .on("start", dragstarted)
-        .on("drag", dragged)
-        .on("end", dragended);
-}
-
-    // Add the tooltip element to the graph
     let div = d3.select("body").append("div")
         .attr("class", "tooltip")
         .style("opacity", 0);
 
-    // Define the tooltip functions
     const addTooltip = (hoverTooltip, d, x, y) => {
         div.transition()
             .duration(200)
@@ -121,68 +123,58 @@ function drag(simulation) {
             .style("left", `${x}px`)
             .style("top", `${y - 28}px`);
     };
-    
+
     const removeTooltip = () => {
         div.transition()
             .duration(500)
             .style("opacity", 0);
     };
 
-    // Define the nodeHoverTooltip function
     const nodeHoverTooltip = (d) => {
         return `
-            <strong>URL:</strong> ${d.data.name}<br>
+            <strong>URL:</strong> ${d.data.url}<br>
             <strong>Additional Info:</strong> Some info here
         `;
     };
 
+    function formatText(d) {
+        let formattedText = d.data.name.length > g ? d.data.name.substring(0, g) + "..." : d.data.name;
+        let rectWidth = formattedText.length * f;
+        return { formattedText, rectWidth };
+    }
 
-// Function to format text and calculate width
-function formatText(d) {
-const maxLength = 20; // Maximum number of characters to display
-let formattedText = d.data.name.length > maxLength ? d.data.name.substring(0, maxLength) + "..." : d.data.name;
-let rectWidth = formattedText.length * 8; // Approximate width based on character count
-return { formattedText, rectWidth };
-}
+    node.each(function(d) {
+        const { formattedText, rectWidth } = formatText(d);
+        const group = d3.select(this);
 
-// Append rectangles and text to the node groups
-node.each(function(d) {
-const { formattedText, rectWidth } = formatText(d);
-const group = d3.select(this);
+        group.append("rect")
+            .attr("width", rectWidth)
+            .attr("height", c)
+            .attr("fill", color(d))
+            .attr("x", -rectWidth / 2)
+            .attr("y", -c / 2);
 
-// Append rectangle
-group.append("rect")
-    .attr("width", rectWidth)
-    .attr("height", 20)
-    .attr("fill", color)
-    .attr("x", -rectWidth / 2)
-    .attr("y", -10);
+        group.append("text")
+            .attr('font-family', 'Raleway', 'Helvetica Neue, Helvetica')
+            .attr("font-size", 10)
+            .attr("fill", "white")
+            .attr("text-anchor", "middle")
+            .attr("dy", ".35em")
+            .text(formattedText);
+    });
 
-// Append text
-group.append("text")
-    .attr('font-family', 'Raleway', 'Helvetica Neue, Helvetica')
-    .attr("font-size", 10)
-    .attr("fill", "white")
-    .attr("text-anchor", "middle")
-    .attr("dy", ".35em")
-    .text(formattedText);
-});
+    simulation.on("tick", () => {
+        link
+            .attr("x1", d => d.source.x)
+            .attr("y1", d => d.source.y)
+            .attr("x2", d => d.target.x)
+            .attr("y2", d => d.target.y);
 
-simulation.on("tick", () => {
-// Update positions of links, rectangles, and text
-link
-    .attr("x1", d => d.source.x)
-    .attr("y1", d => d.source.y)
-    .attr("x2", d => d.target.x)
-    .attr("y2", d => d.target.y);
+        node
+            .attr("transform", d => `translate(${d.x}, ${d.y})`);
+    });
 
-node
-    .attr("transform", d => `translate(${d.x}, ${d.y})`);
-});
-
-    // Hide the tooltip when clicking anywhere else on the body
     d3.select("body").on("click", () => {
         removeTooltip();
     });
 })();
-
