ce5167753ca71ce2bf779ec295f768e9f1ca1d3f
joshirishi <joshi.rishikesh@gmail.com>
2023-12-04 10:44:28 +0530

    update
    now have curved arrows showing ... will scrape larger set and see if I get the double directional arrows

--- src/patent_style.js
+++ src/patent_style.js
@@ -86,57 +86,48 @@ const combinedGraphData = {
     const graphGroup = svg.append("g");
     
     const simulation = d3.forceSimulation(graphData.nodes)
-    .force("link", d3.forceLink(graphData.links).id(d => d.id))
-    .force("charge", d3.forceManyBody())
-    .force("center", d3.forceCenter(width / 2, height / 2));
+    .force("link", d3.forceLink(graphData.links)
+        .id(d => d.id)
+        .distance(100) // Increase this value to spread out the nodes more
+    )
+    .force("charge", d3.forceManyBody().strength(-400)) // Increase the magnitude of the negative value to increase repulsion
+    .force("center", d3.forceCenter(width / 2, height / 2))
+    .force("collide", d3.forceCollide().radius(d => 20)); // Adjust the radius for collision detection
 
-    // Define the arrowheads
+
+    // Define the arrowheads with no fill for the marker itself
     svg.append("defs").selectAll("marker")
-        .data(["end"])
-        .enter().append("marker")
-        .attr("id", String)
-        .attr("viewBox", "0 -5 10 10")
-        .attr("refX", 15) // Position of the arrowhead
-        .attr("refY", 0)
-        .attr("markerWidth", 6)
-        .attr("markerHeight", 6)
-        .attr("orient", "auto")
-        .append("path")
-        .attr("d", "M0,-5L10,0L0,5")
-        .attr("fill", "black");
+    .data(["end"])      // Different link/path types can be defined here
+    .enter().append("marker")
+    .attr("id", String)
+    .attr("viewBox", "0 -5 10 10")
+    .attr("refX", 15)   // Must be tuned
+    .attr("refY", 0)
+    .attr("markerWidth", 6)
+    .attr("markerHeight", 6)
+    .attr("orient", "auto")
+    .append("path")
+    .attr("d", "M0,-5L10,0L0,5")
+    .attr("stroke", "none"); // No fill for the path inside the marker
 
+
+    
+// Arrow color and opacity
+const arrowColor = "grey";
+const arrowOpacity = 0.6;
+
+// Create the links as paths and set the stroke without a fill
     const link = graphGroup.append("g")
         .attr("class", "links")
         .selectAll("path")
         .data(graphData.links)
         .enter().append("path")
-        .attr("stroke", "black")  // Ensure the stroke is a visible color
+        .attr("stroke", "grey")  // Set the stroke to grey
+        .attr("stroke-opacity", 0.6) // Set the opacity to 0.6
         .attr("stroke-width", 2)
+        .attr("fill", "none") // No fill for the path
         .attr("marker-end", "url(#end)");
-
-
-
-    const text = graphGroup.append("g")
-        .attr("class", "texts")
-        .selectAll("text")
-        .data(graphData.nodes)
-        .enter().append("text")
-        .attr("dx", 12) // Offset from the node center
-        .attr("dy", ".35em") // Vertical offset to align text
-        .text(d => d.name); // Assuming each node data has a `name` property
-
-
-    const node = graphGroup.append("g")
-        .attr("class", "nodes")
-        .selectAll("circle")
-        .data(graphData.nodes)
-        .enter().append("circle")
-        .attr("r", 5)
-        .call(d3.drag()
-            .on("start", dragstarted)
-            .on("drag", dragged)
-            .on("end", dragended));
-
+ 
     function dragstarted(event, d) {
         if (!event.active) simulation.alphaTarget(0.3).restart();
         d.fx = d.x;
@@ -160,18 +151,46 @@ simulation.on("tick", () => {
     link.attr("d", d => {
         const dx = d.target.x - d.source.x,
               dy = d.target.y - d.source.y,
-              dr = Math.sqrt(dx * dx + dy * dy) * 2; // Increase the 2 to make more curvature
-        return `M ${d.source.x} ${d.source.y} A ${dr} ${dr} 0 0 1 ${d.target.x} ${d.target.y}`;
+              dr = Math.sqrt(dx * dx + dy * dy) * 2; // Control the amount of curvature
+        // Use the 'A' command for a curved path
+        return `M ${d.source.x} ${d.source.y} A ${dr} ${dr} 0 0,1 ${d.target.x} ${d.target.y}`;
     });
 
-    // Update node positions
     node
-        .attr("cx", d => d.x)
-        .attr("cy", d => d.y);
+    .attr("x", d => d.x - nodeWidth / 2)
+    .attr("y", d => d.y - nodeHeight / 2);
 
-    // Update text label positions
-    text
-        .attr("x", d => d.x + 10) // Offset the text a bit to the right of the node
-        .attr("y", d => d.y);
+text
+    .attr("x", d => d.x - nodeWidth / 2 + 5)
+    .attr("y", d => d.y + nodeHeight / 4);
 });
+
+const nodeWidth = 100;
+const nodeHeight = 20;
+const node = graphGroup.append("g")
+    .attr("class", "nodes")
+    .selectAll("rect")
+    .data(graphData.nodes)
+    .enter().append("rect")
+    .attr("width", nodeWidth)
+    .attr("height", nodeHeight)
+    .attr("fill", "blue") // Set the fill to teal color
+    .attr("stroke", "#333") // Optional: add a stroke to the rectangles
+    .call(d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended));
+
+        // When appending text, handle the overflow and positioning
+// Text inside the rectangles
+const text = graphGroup.append("g")
+    .attr("class", "texts")
+    .selectAll("text")
+    .data(graphData.nodes)
+    .enter().append("text")
+    .attr("x", d => d.x - nodeWidth / 2 + 5) // Position text inside the rectangle
+    .attr("y", d => d.y + nodeHeight / 4) // Vertically center text within the rectangle
+    .attr("fill", "white") // Set text color to contrast with teal background
+    .text(d => d.name.length > 20 ? d.name.substring(0, 20) + "..." : d.name);
+
 })();
